7184d328712d98ec5d2e0eca5e8b30a459700500
Krystal Foster <kmfoster777@gmail.com>
2018-09-21 11:33:53 -0700

    add amenities page and webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,11 @@ module.exports = {
       inject: 'body',
       template: './src/index.html',
       filename: 'index.html',
-    })
+    }),
+    new HtmlWebpackPlugin({
+    template: './src/amenities.html',
+    filename: 'amenities.html'
+  }),
 
   ]
 
